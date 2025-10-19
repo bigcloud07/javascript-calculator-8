7b594d9dda6766e4d1978310dfa77d3c1b0eab8f
bigcloud07 <dnsxo0712@gmail.com>
2025-10-19 19:06:08 +0900

    feat: 기본 구분자 처리 기능

--- src/stringInputFunction.js
+++ src/stringInputFunction.js
@@ -1,8 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
 
+const DELIMITER_REGEX = /,|:/;
+
+function calculateDelimiterSum(input) {
+        const segments = input.split(DELIMITER_REGEX);
+        return segments
+            .map((s) => Number(s))
+            .reduce((sum, n) => sum + n, 0)
+    }
 
 export default function stringInputFunction() {
-    Console.readLine('문자열을 입력해주세요: ', (input) => {
-        Console.print(`입력하신 문자열은 ${input}입니다.`);
-    });
+  Console.readLine('문자열을 입력해주세요: ', (input) => {
+    Console.print(`입력하신 문자열은 ${input}입니다.`);
+
+    const result = calculateDelimiterSum(input);
+    Console.print(`결과는 ${result}입니다.`);
+  });
 }
