545265d8fc48922423c3241ba74f0cd6e74498a8
bigcloud07 <dnsxo0712@gmail.com>
2025-10-20 01:36:55 +0900

    feat: 음수 입력 예외 처리 기능 추가

--- src/stringInputFunction.js
+++ src/stringInputFunction.js
@@ -2,23 +2,27 @@ import { Console } from '@woowacourse/mission-utils';
 
 const DEFAULT_DELIMITER = /,|:/;
 
+function validateNegativeNumbers(n) {
+    const negatives = n.filter((n) => n < 0);
+    if(negatives.length > 0){
+        throw new Error('[ERROR] 음수는 입력하지 마세요');
+    }
+}
+
 function calculateDelimiterSum(input) {
   if (input.startsWith('//')) {
     const nlIndex = input.indexOf('\\n');
     const customDelimiter = input.slice(2, nlIndex);
     const resultSliceString = input.slice(nlIndex + 2);
 
-
-    const resultSegment = resultSliceString.split(customDelimiter);
-    return resultSegment
-      .map((s) => Number(s))
-      .reduce((sum, n) => sum + n, 0);
+    const resultSegment = resultSliceString.split(customDelimiter).map(Number);
+    validateNegativeNumbers(resultSegment)
+    return resultSegment.reduce((sum, n) => sum + n, 0);
   }
 
-  const segments = input.split(DEFAULT_DELIMITER);
-  return segments
-    .map((s) => Number(s))
-    .reduce((sum, n) => sum + n, 0);
+  const segments = input.split(DEFAULT_DELIMITER).map(Number);
+  validateNegativeNumbers(segments)
+  return segments.reduce((sum, n) => sum + n, 0);
 }
 
 export default async function stringInputFunction() {
@@ -26,5 +30,5 @@ export default async function stringInputFunction() {
   Console.print(`입력하신 문자열은 ${input}입니다.`);
 
   const result = calculateDelimiterSum(input);
-  Console.print(`결과 : ${result}`);
+  return Console.print(`결과 : ${result}`);
 }
