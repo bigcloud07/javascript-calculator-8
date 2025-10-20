9b06acb590b63840426f742be7410755befc09bc
bigcloud07 <dnsxo0712@gmail.com>
2025-10-20 12:10:12 +0900

    feat: NaN 및 구분자 사이 공백값 예외 처리 기능

--- src/stringInputFunction.js
+++ src/stringInputFunction.js
@@ -4,31 +4,52 @@ const DEFAULT_DELIMITER = /,|:/;
 
 function validateNegativeNumbers(n) {
     const negatives = n.filter((n) => n < 0);
-    if(negatives.length > 0){
-        throw new Error('[ERROR] 음수는 입력하지 마세요');
+    if (negatives.length > 0) {
+        throw new Error('[ERROR]음수는 입력하지 마세요');
+    }
+}
+
+function validateNaNValue(n){
+    const hasNaN = n.some((s) => Number.isNaN(s));
+    if(hasNaN){
+        throw new Error('[ERROR]입력을 확인해주세요');
+    }
+}
+
+function validateWhiteSpace(n) {
+    const hasWhiteSpace = n.some((n) => n == 0);
+    if (hasWhiteSpace){
+        throw new Error('[ERROR]구분자 사이에 값을 입력해주세요')
     }
 }
 
 function calculateDelimiterSum(input) {
-  if (input.startsWith('//')) {
-    const nlIndex = input.indexOf('\\n');
-    const customDelimiter = input.slice(2, nlIndex);
-    const resultSliceString = input.slice(nlIndex + 2);
+    if (input.startsWith('//')) {
+        const nlIndex = input.indexOf('\\n');
+        const customDelimiter = input.slice(2, nlIndex);
+        const resultSliceString = input.slice(nlIndex + 2);
 
-    const resultSegment = resultSliceString.split(customDelimiter).map(Number);
-    validateNegativeNumbers(resultSegment)
-    return resultSegment.reduce((sum, n) => sum + n, 0);
-  }
-
-  const segments = input.split(DEFAULT_DELIMITER).map(Number);
-  validateNegativeNumbers(segments)
-  return segments.reduce((sum, n) => sum + n, 0);
+        const resultSegment = resultSliceString.split(customDelimiter).map(Number);
+        console.log(resultSegment)
+        validateNegativeNumbers(resultSegment)
+        validateNaNValue(resultSegment)
+        validateWhiteSpace(resultSegment)
+        return resultSegment.reduce((sum, n) => sum + n, 0);
+    }
+    else {
+        const segments = input.split(DEFAULT_DELIMITER).map(Number);
+        console.log(segments)
+        validateNegativeNumbers(segments)
+        validateNaNValue(segments)
+        validateWhiteSpace(segments)
+        return segments.reduce((sum, n) => sum + n, 0);
+    }
 }
 
 export default async function stringInputFunction() {
-  const input = await Console.readLineAsync('문자열을 입력해주세요: ');
-  Console.print(`입력하신 문자열은 ${input}입니다.`);
+    const input = await Console.readLineAsync('문자열을 입력해주세요: ');
+    Console.print(`입력하신 문자열은 ${input}입니다.`);
 
-  const result = calculateDelimiterSum(input);
-  return Console.print(`결과 : ${result}`);
+    const result = calculateDelimiterSum(input);
+    return Console.print(`결과 : ${result}`);
 }
